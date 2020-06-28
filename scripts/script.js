74d54576dd78f7d2aa637919c48865c885655b34
German Shtey <gvshteynardt@gmail.com>
2020-06-28 23:15:01 +0300

    feat: delete todo

--- scripts/script.js
+++ scripts/script.js
@@ -10,14 +10,23 @@ const todosForm = document.querySelector('.todos__form');
 const todoTemplate = document.querySelector('.todo-template');
 const todosInput = todosForm.querySelector('.todos__input');
 
-//получаем содержимое template и клонируем, выбираем todo__text и вставляем тест из массива, вставяляем копию каждой todo в начало todos__list
 
+//получаем содержимое template и клонируем, выбираем todo__text и вставляем тест из массива, вставяляем копию каждой todo в начало todos__list
 function addToDo(text){
   const todo = todoTemplate.content.cloneNode(true);
   todo.querySelector('.todo__text').textContent = text;
-  todosList.prepend(todo);
 
-}//при загрузки страницы элементы массива обрабатываются function addToDo()
+  todo.querySelector('.todo__btn_type_delete').addEventListener('click', deleteToDo);
+
+  todosList.prepend(todo);
+}
+
+function deleteToDo(evt){
+  const todo = evt.target.closest('.todo');
+  todo.remove();
+}
+
+//при загрузки страницы элементы массива обрабатываются function addToDo()
 todos.forEach(text => {
   addToDo(text);
 })
@@ -30,3 +39,6 @@ todosForm.addEventListener('submit', evt => {
   addToDo(text);
   todosInput.value = ''; //при добавлении todo input остается пустой
 })
+
+
+
